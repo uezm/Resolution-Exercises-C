7e0d1bbc140ec2638451f05dcc79a0390c502f93
DANIEL UEZ MARTINS <daniel.uez@gmail.com>
2023-11-14 12:01:19 -0300

    feat(Chapter-1): Added Exercise 1 to 5 from Chapter 1

---
+++ first-steps/Chapter-1/Exercise-3.c
@@ -0,0 +1,12 @@
+/* 3) Write a program that reads a number integer and then print
+the message ?Value read:?, followed by the integer value. Use only one
+printf() command. */
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    printf("Value read: %d", num);
+    return 0;
+}
---
+++ first-steps/Chapter-1/Exercise-5.c
@@ -0,0 +1,11 @@
+/* 5) Write a program that reads a value like float and then print it
+using the ?%d? operator. See what happened. */
+#include <stdio.h>
+
+int main()
+{
+    float num;
+    scanf("%f", &num);
+    printf("%d", num);
+    return 0;
+}
