1b0b44d22c93166c2efd90e3aa0f75fd2e7826a6
DANIEL UEZ MARTINS <daniel.uez@gmail.com>
2023-11-14 12:01:19 -0300

    feat(Chapter-2): Added Exercise 1 to 5 from Chapter 2

---
+++ first-steps/Chapter-1/Exercise-1.c
@@ -0,0 +1,9 @@
+/* 1) Create a program that writes the ?Program Start? and ?End?
+messages on the screen, one on each line, using just one command printf(). */
+#include <stdio.h>
+
+int main()
+{
+    printf("Program Start\nEnd");
+    return 0;
+}
---
+++ first-steps/Chapter-1/Exercise-2.c
@@ -0,0 +1,10 @@
+/* 2) Write a program that reads a number entire and then print it. */
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    printf("%d", num);
+    return 0;
+}
